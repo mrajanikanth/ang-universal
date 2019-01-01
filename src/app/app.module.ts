6004741d18a7134ad8b3dca462b81ee852022030
mrajanikanth <mrajanikanth@live>
2019-01-01 17:59:11 +0000

    upto routing exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { HomeComponent } from './components/home/home.component';
+import { Error404Component } from './components/error404/error404.component';
+import {AppRoutingModule} from './app-routing.module';
+
+export const DECLARATIONS = [
+  HomeComponent,
+  Error404Component
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Error404Component,
+    DECLARATIONS
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    CoreModule.forClient()
+    // CoreModule.forServer()
   ],
   providers: [],
   bootstrap: [AppComponent]
